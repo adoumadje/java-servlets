4d761cf15ff43bb69704cd321ba6a51c581a9368
adoumadje <adoumadjecaleb@gmail.com>
2025-11-17 10:35:03 +0100

    servlet crud app - delete

--- dynamic-crud-app/src/main/java/com/adoumadje/DeleteUserServlet.java
+++ dynamic-crud-app/src/main/java/com/adoumadje/DeleteUserServlet.java
@@ -1,4 +1,52 @@
 package com.adoumadje;
 
-public class DeleteUserServlet  {
+import com.adoumadje.utils.Constants;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@WebServlet("/delete-user")
+public class DeleteUserServlet extends HttpServlet {
+    private Connection connection;
+
+    @Override
+    public void init() throws ServletException {
+        try {
+            Class.forName(Constants.MYSQL_DRIVER);
+            this.connection = DriverManager.getConnection(Constants.DB_URL, Constants.DB_USER, Constants.DB_PASS);
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Long id = Long.valueOf(req.getParameter("id"));
+            String sql = "DELETE FROM users WHERE id = ?";
+            PreparedStatement preparedStatement = this.connection.prepareStatement(sql);
+            preparedStatement.setLong(1, id);
+            preparedStatement.execute();
+            resp.sendRedirect(req.getContextPath() + "/list-users");
+        } catch (SQLException | NumberFormatException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        try {
+            this.connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
